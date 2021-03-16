a17f2533f9db6da6ba4a444f3353ed2a2c8616da
ssaboorr <sabooransari777@gmail.com>
2021-03-17 00:59:34 +0530

    initial commit

--- DjangoCRUD/DjangoCRUD/urls.py
+++ DjangoCRUD/DjangoCRUD/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from enroll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('enroll/', include('enroll.urls')),
+    path('',views.add_display,name="addanddisplay" ),
+    path('delete/<int:id>/', views.delete_data, name="delete"),
+    path('<int:id>/', views.update_edit, name="update"),
+
 ]
+
+
